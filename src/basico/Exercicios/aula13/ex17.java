b0ecce9aa74680eed1415bc5500b33da30b41580
marcisg <marcig2701@gmail.com>
2025-08-17 13:06:50 -0300

    atualização

--- src/basico/Exercicios/aula13/ex17.java
+++ src/basico/Exercicios/aula13/ex17.java
@@ -1,5 +1,6 @@
 package basico.Exercicios.aula13;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ex17{
@@ -8,32 +9,40 @@ public class ex17{
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Quantos metros quadrados de área a ser pintado?");
-        double area = scan.nextDouble();
-        double litrostot = area / 6.0;
+        double metrosquadrados = scan.nextDouble();
+        double metrosporlitro = metrosquadrados/6;
 
-        double latas18 = Math.ceil(litrostot / 18.0);
-        double precoLatas18 = latas18 * 80.0;
+        int latasde18 = (int)Math.ceil(metrosporlitro/18);
+        double valorlatasde18 = (latasde18 * 80);
 
-        double galoes36 = Math.ceil(litrostot / 3.6);
-        double precoGaloes36 = galoes36 * 25.0;
+        int galoesde3e6 = (int)Math.ceil(metrosporlitro/3.6);
+        double valorgaloesde3e6 = (galoesde3e6 * 25);
 
-        int latasMix = (int)(litrostot / 18);
-        double restanteLitros = litrostot - (latasMix * 18);
-        int galoesMix = (int)Math.ceil(restanteLitros / 3.6);
-        double precoMix = (latasMix * 80.0) + (galoesMix * 25.0);
+        double metroscomfolga = metrosporlitro * 1.10;
+        int latasrestantes = (int)metroscomfolga/18;
+        int litrosrestantes = (int)metroscomfolga - (latasrestantes * 18);
+        int latasde3e6 = (int)Math.ceil(litrosrestantes/3.6) ;
+        double valorfinal =  (latasde18 * 80) + (latasde3e6 * 25);
 
 
-        System.out.println("\n=== Apenas latas de 18 litros ===");
-        System.out.println("Quantidade de latas: " + (int)latas18);
-        System.out.println("Preço: R$ " + precoLatas18);
+        System.out.println("===== APENAS LATAS DE 18L SEM FOLGA ===== ");
+        System.out.println("Quantidade de latas: " + latasde18);
+        System.out.println("Valor final: "+ valorlatasde18 );
+        System.out.println();
+        System.out.println("===== APENAS LATAS DE 3,6L SEM FOLGA ===== ");
+        System.out.println("Quantidade de latas: " + galoesde3e6);
+        System.out.println("Valor final: "+ valorgaloesde3e6);
+        System.out.println();
+        System.out.println("===== USANDO MISTURA DE DOIS TIPOS DE LATAS E COM FOLGA DE 10% ===== ");
+        System.out.println("Quantidade de latas DE 18L: " + latasde18);
+        System.out.println("Quantidade de latas DE 3,6L: " + latasde3e6);
+        System.out.println("Valor final: "+ valorfinal);
+        System.out.println();
+        //System.out.println("Você precisa comprar " + latasde18 + " lata(s) de tinta de 18L com o valor de 80,00R$ e " + latasde3e6 + " lata(s) de 3,6L por 25,00R$, totalizando um valor final de " + valorfinal);
 
-        System.out.println("\n=== Apenas galões de 3,6 litros ===");
-        System.out.println("Quantidade de galões: " + (int)galoes36);
-        System.out.println("Preço: R$ " + precoGaloes36);
 
-        System.out.println("\n=== Mistura de latas e galões (preço menor) ===");
-        System.out.println("Latas: " + latasMix);
-        System.out.println("Galões: " + galoesMix);
-        System.out.println("Preço: R$ " + precoMix);
+
+
+
 }
 }
